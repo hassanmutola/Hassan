56b5ad562850f5789f368a54a11c7c6b9d5f3e1c
hassan mutola <hassanmx57@gmail.com>
2019-12-04 08:47:20 +0200

    Initial commit

---
+++ src/hassan/Hassan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hassan;
+
+/**
+ *
+ * @author Hassan
+ */
+public class Hassan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("OLA hassan"); // TODO code application logic here
+    }
+    
+}
